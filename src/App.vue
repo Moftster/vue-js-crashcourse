8258fbb67affe8e157c99e51c02c19e3c5118b0f
David Moftakhar <david@sellmysnaps.com>
2019-11-26 19:11:54 +0000

    Adding new items to list.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
+    <Header />
+    <AddToDo v-on:add-todo="addToDo"/>
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
 
+import Header from './components/layouts/header';
+
 import Todos from './components/Todos';
+
+import AddToDo from './components/AddToDo';
 
 export default {
   name: 'app',
   components: {
-    Todos
+    Header,
+    Todos,
+    AddToDo
   },
   data() {
     return {
@@ -38,6 +46,9 @@ export default {
   methods: {
     deleteTodo(id) {
       this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addToDo(newToDo) {
+      this.todos = [...this.todos, newToDo];
     }
   }
 }
@@ -55,6 +66,19 @@ body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
   padding: 10%;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
 }
 
 </style>
---
+++ src/components/AddToDo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <form @submit="addToDo" >
+            <input type="text" v-model="title" name="title" placeholder="Add ToDo...">
+            <input type="submit" value="Submit" class="btn">
+        </form>
+    </div>
+</template>
+
+<script>
+import uuid from 'uuid';
+export default {
+    name:"AddToDo",
+    data() {
+        return {
+            title: ''
+        }
+    }, methods: {
+        addToDo(e) {
+            e.preventDefault();
+            const newToDo = {
+                id: uuid.v4(),
+                title: this.title,
+                completed: false
+            }
+        // Send up to parent
+        this.$emit('add-todo', newToDo);
+        }
+    }
+}
+</script>
+
+<style scoped>
+form {
+    display: flex;
+}
+
+input[type="text"] {
+    flex: 10;
+    padding: 5px;
+}
+</style>
