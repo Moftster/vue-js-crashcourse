ca83577434116315d1846e7567a2a194cda0aea6
David Moftakhar <david@sellmysnaps.com>
2019-11-26 07:41:23 +0000

    Delete item in list.

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -3,6 +3,8 @@
         <p>
             <input type="checkbox" v-on:change="markComplete">
             {{todo.title}}
+            <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+            
         </p>
     </div>
   
