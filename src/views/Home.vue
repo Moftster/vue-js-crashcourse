6263a291bb61390fc8e0820ec02684b086f3e98e
David Moftakhar <david@sellmysnaps.com>
2019-11-28 08:05:36 +0000

    Added vue router.

---
+++ src/views/Home.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="app">
+    <Header />
+    <AddToDo v-on:add-todo="addToDo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+
+import Header from '../components/layouts/header';
+import Todos from '../components/Todos';
+import AddToDo from '../components/AddToDo';
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Todos,
+    AddToDo
+  },
+  data() {
+    return {
+      todos: [
+       
+      ]
+    }
+  },
+
+  methods: {
+    deleteTodo(id) {
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.todos = this.todos.filter(todo => todo.id !== id, res.data));
+      
+    },
+    addToDo(newToDo) {
+      const {title, completed} = newToDo;
+
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+      .then(res => this.todos = [...this.todos, res.data]);
+      
+    }
+  },
+  created() {
+    // eslint-disable-next-line no-console
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(res => this.todos = res.data);
+      // .catch(err => console.log(err));
+  }
+}
+</script>
+
+<style>
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+  padding: 10%;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
+}
+
+</style>
+
